18b82bb4ab68765d8f53fc17342a0b416834b15f
Berishev Ravil <quadraticteam@gmail.com>
2019-06-30 18:15:58 +0300

    implementation + tests

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,359 @@
+import * as faker from 'faker';
+import LINQ from '../';
+
+class TestAType {}
+class TestBType {}
+
+function generateCard(index: number = 0) {
+  const userCard = faker.helpers.userCard();
+  return {
+    ...userCard,
+    age: faker.random.number({ min: 0, max: 30 }),
+    id: faker.random.uuid(),
+    rating: faker.random.number({ min: 0, max: 100 }),
+    types: index % 2 === 0 ? new TestAType() : new TestBType(),
+  };
+}
+
+function getLinq(count: number = 10) {
+  const data = Array(count)
+    .fill(null)
+    .map((m, i) => generateCard(i));
+  return LINQ.from(data);
+}
+
+describe('check linq', () => {
+  test('instance of linq', () => {
+    const linq1 = LINQ.from([1]);
+    const linq2 = linq1.skip(1);
+    expect(LINQ.from([1])).not.toBe(LINQ.from([1]));
+    expect(linq1).not.toBe(linq2);
+    expect(LINQ.from([]) === LINQ.from([])).toBeFalsy();
+  });
+
+  test('clone', () => {
+    const linq1 = LINQ.from([1, 2, 3]);
+    const cloned = linq1.clone();
+
+    expect(cloned).toEqual(linq1);
+    expect(cloned === linq1).toBeFalsy();
+  });
+
+  test('where', () => {
+    const linq = getLinq();
+    expect(linq.where(m => m.age % 2 === 0)).toEqual(linq.filter(m => m.age % 2 === 0));
+  });
+
+  test('whereWithAccum', () => {
+    const linq = getLinq();
+
+    const where = linq.whereWithAccum(m => m.age, selected => Math.max(...selected), (m, i, l, max) => m >= max);
+    const max = Math.max(...linq.map(m => m.age));
+    const maxWhere = linq.where(m => m.age === max);
+
+    expect(where).toEqual(maxWhere);
+  });
+
+  test('select', () => {
+    const linq = getLinq();
+
+    const selected = linq.select(m => m.email);
+    const mapped = linq.map(m => m.email);
+
+    expect(selected).toEqual(mapped);
+  });
+
+  test('selectMany', () => {
+    const linq = LINQ.from([getLinq(), getLinq(), getLinq()]);
+
+    const selected1 = linq.selectMany();
+    expect(selected1).toEqual([...linq[0], ...linq[1], ...linq[2]]);
+
+    const selected2 = linq.selectMany(m => m.select(m => m.id));
+    expect(selected2).toEqual([
+      ...linq[0].select(m => m.id),
+      ...linq[1].select(m => m.id),
+      ...linq[2].select(m => m.id),
+    ]);
+  });
+
+  test('take', () => {
+    const linq = getLinq(50);
+
+    const take = linq.take(20);
+    expect(take.length).toBe(20);
+    expect(take).toEqual(linq.slice(0, 20));
+  });
+
+  test('skip', () => {
+    const linq = getLinq(100);
+
+    const skip = linq.skip(20);
+    expect(skip.length).toBe(80);
+    expect(skip).toEqual(linq.slice(20));
+  });
+
+  test('count', () => {
+    const linq = getLinq(100);
+
+    const count1 = linq.count();
+    expect(count1).toBe(100);
+
+    const count2 = linq.count(m => m.age % 2 === 0);
+    expect(count2).toBe(linq.where(m => m.age % 2 === 0).count());
+  });
+
+  test('indexWhere', () => {
+    const linq = getLinq();
+
+    const indexWhere = linq.indexWhere(m => m.age % 2 === 0);
+    expect(indexWhere).toEqual(linq.select((m, i) => (m.age % 2 === 0 ? i : null)).notNull());
+  });
+  test('elementsAtIndex', () => {
+    const linq = getLinq();
+
+    const elementsAtIndex = linq.elementsAtIndex([0, 5, 8, 12]);
+    expect(elementsAtIndex.length).toBe(4);
+    expect(elementsAtIndex).toEqual([linq[0], linq[5], linq[8], linq[12]]);
+  });
+
+  test('notNull', () => {
+    const linq = getLinq(100);
+
+    const notNull = linq.select((m, i) => (i % 2 === 0 ? m : null)).notNull();
+    expect(notNull.length).toBe(50);
+    expect(notNull).toEqual(linq.where((m, i) => i % 2 === 0));
+  });
+
+  test('notEmpty', () => {
+    const linq = getLinq(100);
+
+    const notEmpty = linq.select((m, i) => (i % 2 === 0 ? m : '')).notEmpty();
+    expect(notEmpty.length).toBe(50);
+    expect(notEmpty).toEqual(linq.where((m, i) => i % 2 === 0));
+  });
+
+  test('first', () => {
+    const linq = getLinq();
+
+    const first1 = linq.first();
+    expect(first1).toBe(linq[0]);
+
+    const first2 = linq.first(m => m.age % 2 === 0);
+    expect(first2).toBe(linq.where(m => m.age % 2 === 0).first());
+  });
+
+  test('last', () => {
+    const linq = getLinq();
+
+    const last1 = linq.last();
+    expect(last1).toBe(linq[linq.length - 1]);
+
+    const last2 = linq.last(m => m.age % 2 === 0);
+    expect(last2).toBe(linq.where(m => m.age % 2 === 0).last());
+  });
+
+  test('distinct', () => {
+    const linq = getLinq(100);
+
+    const distinct1 = linq.select(m => m.age).distinct();
+    expect(distinct1.length).toBe(new Set(linq.select(m => m.age)).size);
+
+    const distinct2 = linq.distinct(m => m.age);
+    const addedAges: number[] = [];
+    const testDistinct2 = linq.where(m => {
+      if (addedAges.indexOf(m.age) === -1) {
+        addedAges.push(m.age);
+        return true;
+      }
+      return false;
+    });
+    expect(distinct2).toEqual(testDistinct2);
+  });
+
+  test('max', () => {
+    const linq = getLinq();
+
+    const max = linq.max(m => m.age);
+    const maxValue = Math.max(...linq.select(m => m.age));
+    expect(max).toEqual(linq.where(m => m.age === maxValue));
+  });
+
+  test('maxValue', () => {
+    const linq = getLinq();
+
+    const maxValue = linq.maxValue(m => m.age);
+    expect(maxValue).toBe(Math.max(...linq.select(m => m.age)));
+  });
+
+  test('min', () => {
+    const linq = getLinq();
+
+    const min = linq.min(m => m.age);
+    const minValue = Math.min(...linq.select(m => m.age));
+    expect(min).toEqual(linq.where(m => m.age === minValue));
+  });
+
+  test('minValue', () => {
+    const linq = getLinq();
+
+    const minValue = linq.minValue(m => m.age);
+    expect(minValue).toBe(Math.min(...linq.select(m => m.age)));
+  });
+
+  test('ofType', () => {
+    const linq = getLinq();
+
+    const ofType1 = linq.ofType(Object);
+    expect(ofType1).toEqual(linq);
+
+    const ofType2 = linq.ofType(TestAType, m => m.types);
+    expect(ofType2).toEqual(linq.where(m => m.types instanceof TestAType));
+
+    const ofType3 = linq.ofType([TestAType, TestBType], m => m.types);
+    expect(ofType3).toEqual(linq);
+  });
+
+  test('orderByAscending', () => {
+    const linq1 = LINQ.from([4, 2, 1, 5, 8, -1]);
+    const orderByAscending1 = linq1.orderByAscending();
+    expect(orderByAscending1).toEqual([-1, 1, 2, 4, 5, 8]);
+
+    const linq2 = getLinq();
+
+    const orderByAscending2 = linq2.orderByAscending(m => m.age);
+    const bySort2 = linq2.sort((a, b) => {
+      if (a.age > b.age) return 1;
+      if (a.age < b.age) return -1;
+      return 0;
+    });
+    expect(orderByAscending2).toEqual(bySort2);
+  });
+
+  test('orderByDescending', () => {
+    const linq1 = LINQ.from([4, 2, 1, 5, 8, -1]);
+    const orderByDescending1 = linq1.orderByDescending();
+    expect(orderByDescending1).toEqual([8, 5, 4, 2, 1, -1]);
+
+    const linq2 = getLinq();
+
+    const orderByDescending2 = linq2.orderByDescending(m => m.age);
+    const bySort2 = linq2.sort((a, b) => {
+      if (a.age < b.age) return 1;
+      if (a.age > b.age) return -1;
+      return 0;
+    });
+    expect(orderByDescending2).toEqual(bySort2);
+  });
+
+  test('sum', () => {
+    const linq1 = LINQ.from([4, 2, 1, 5, 8, -1]);
+    const sum1 = linq1.sum();
+    expect(sum1).toBe(linq1.reduce((a, b) => a + b, 0));
+
+    const linq2 = getLinq();
+    const sum2 = linq2.sum(m => m.age);
+    expect(sum2).toBe(linq2.select(m => m.age).reduce((a, b) => a + b, 0));
+  });
+
+  test('except', () => {
+    const linq1 = LINQ.from([4, 2, 1, 5, 8, -1]);
+    const linq2 = LINQ.from([4, -100, 7, 5, 8, -1]);
+    const expect1 = linq1.except(linq2);
+    expect(expect1).toEqual([2, 1]);
+
+    const linq3 = getLinq(100);
+    const linq4 = getLinq(100);
+    expect(linq3.except(linq4)).toEqual(linq3);
+
+    const expect3 = linq3.except(linq4, m => m.age);
+    expect(expect3).toEqual(linq3.where(m => !linq4.contains(m, k => k.age)));
+  });
+
+  test('groupBy', () => {});
+
+  test('contains', () => {
+    const linq1 = LINQ.from(['hey', 'bro']);
+    expect(linq1.contains('hey')).toBeTruthy();
+    expect(linq1.contains('hello')).toBeFalsy();
+
+    const linq2 = getLinq(100);
+    expect(linq2.contains(linq2[0])).toBeTruthy();
+
+    let card = generateCard();
+    while (!linq2.first(m => m.age === card.age)) {
+      card = generateCard();
+    }
+    expect(linq2.contains(card)).toBeFalsy();
+    expect(linq2.contains(card, m => m.age)).toBeTruthy();
+  });
+
+  test('containsAll', () => {
+    const linq1 = LINQ.from(['hey', 'bro']);
+    expect(linq1.containsAll(['hey', 'bro'])).toBeTruthy();
+    expect(linq1.containsAll(['hey', 'hello'])).toBeFalsy();
+
+    const linq2 = getLinq(100);
+    expect(linq2.containsAll([linq2.first(), linq2.last()])).toBeTruthy();
+
+    let card1 = generateCard();
+    while (!linq2.first(m => m.age === card1.age)) {
+      card1 = generateCard();
+    }
+
+    let card2 = generateCard();
+    while (!linq2.first(m => m.age === card2.age)) {
+      card2 = generateCard();
+    }
+    expect(linq2.containsAll([card1, card2])).toBeFalsy();
+    expect(linq2.containsAll([card1, card2], m => m.age)).toBeTruthy();
+  });
+
+  test('average', () => {
+    const linq1 = LINQ.from([1, 2, 5, 10]);
+    expect(linq1.average()).toBe(4.5);
+    expect(linq1.average(null, m => m % 2 === 0)).toBe(6);
+
+    const linq2 = getLinq();
+    expect(linq2.average(m => m.age)).toBe(linq2.select(m => m.age).average());
+    expect(linq2.average(m => m.rating, m => m.age > 18)).toBe(
+      linq2
+        .where(m => m.age > 18)
+        .select(m => m.rating)
+        .average(),
+    );
+  });
+
+  test('reverse', () => {
+    const linq = LINQ.from([1, 4, 2, 3, 3]);
+    expect(linq.reverse()).toEqual([3, 3, 2, 4, 1]);
+  });
+
+  test('concat', () => {
+    const linq1 = LINQ.from([1, 4, 3]);
+    const linq2 = LINQ.from([2, 3, 3]);
+    expect(linq1.concat(linq2)).toEqual([1, 4, 3, 2, 3, 3]);
+  });
+
+  test('slice', () => {
+    const linq = LINQ.from([1, 4, 2, 3, 3]);
+    expect(linq.slice(2, 4)).toEqual([2, 3]);
+  });
+
+  test('splice', () => {
+    const linq = getLinq();
+    const cloned = linq.clone();
+    cloned.splice(0, 3);
+    expect(cloned).toEqual(linq.skip(3));
+  });
+
+  test('map', () => {
+    const linq = LINQ.from([{ id: 1 }, { id: 2 }]);
+    expect(linq.map(m => m.id)).toEqual([1, 2]);
+  });
+
+  test('filter', () => {
+    const linq = LINQ.from([{ id: 1 }, { id: 2 }]);
+    expect(linq.filter(m => m.id > 1)).toEqual([{ id: 2 }]);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,241 @@
-export class LINQ extends Array {
-  constructor(...items) {
-    super(...items);
+import * as collection from 'berish-collection';
+
+type CallbackType<T, Response> = (item: T, index: number, linq: LINQ<T>) => Response;
+type CallbackWithAccumType<T, Accum, Response> = (item: T, index: number, linq: LINQ<T>, accum: Accum) => Response;
+type CallbackOnlyItemType<T, Response> = (item: T) => Response;
+
+export default class LINQ<T> extends Array<T> {
+  public static from<T>(items?: T[]): LINQ<T> {
+    const linq = new LINQ<T>();
+    if (items && items.length > 0) linq.push(...items);
+    return linq;
+  }
+
+  public clone(): LINQ<T> {
+    return this.slice(0);
+  }
+
+  public where(whereFunc: CallbackType<T, boolean>): LINQ<T> {
+    const returnValue = this.filter((m, i) => whereFunc(m, i, this));
+    return LINQ.from(returnValue);
+  }
+
+  public whereWithAccum<K, Accum = LINQ<K>>(
+    selectFunc: CallbackType<T, K>,
+    accumFunc: (selected: LINQ<K>, linq: LINQ<T>) => Accum,
+    whereSelectedFunc: CallbackWithAccumType<K, Accum, boolean>,
+  ): LINQ<T> {
+    const selected = this.select(selectFunc);
+    const accum = accumFunc ? accumFunc(selected, this) : ((selected as any) as Accum);
+    return this.where((m, i, linq) => whereSelectedFunc(selected[i], i, selected, accum));
+  }
+
+  public select<K>(selectFunc: CallbackType<T, K>): LINQ<K> {
+    if (!selectFunc) return (this as any) as LINQ<K>;
+    const returnValue = this.map((m, i) => selectFunc(m, i, this));
+    return LINQ.from(returnValue);
+  }
+
+  public selectMany<K>(selectFunc?: CallbackType<T, K[]>): LINQ<K> {
+    const selected = this.select(selectFunc);
+    const returnValue = [].concat(...selected) as K[];
+    return LINQ.from(returnValue);
+  }
+
+  public take(count: number): LINQ<T> {
+    const returnValue = this.slice(0, count);
+    return LINQ.from(returnValue);
+  }
+
+  public skip(count: number): LINQ<T> {
+    const returnValue = this.slice(count);
+    return LINQ.from(returnValue);
+  }
+
+  public count(func?: CallbackType<T, boolean>): number {
+    if (!func) return this.length || 0;
+    return this.where(func).count();
+  }
+
+  public indexWhere(whereFunc?: CallbackType<T, boolean>): LINQ<number> {
+    const selected = this.select<number>((m, i, linq) => (whereFunc(m, i, linq) ? i : null));
+    return selected.notNull();
+  }
+
+  public elementsAtIndex(indexes: number[]): LINQ<T> {
+    return LINQ.from(indexes).select(m => this[m]);
+  }
+
+  public notNull<K>(selectFunc?: CallbackType<T, K>): LINQ<T> {
+    if (selectFunc) return this.where((m, i, linq) => selectFunc(m, i, linq) != null);
+    return this.where(m => m != null);
+  }
+
+  public notEmpty<K>(selectFunc?: CallbackType<T, K>): LINQ<T> {
+    if (selectFunc) return this.where((m, i, linq) => !!selectFunc(m, i, linq));
+    return this.where(m => !!m);
+  }
+
+  public first(whereFunc?: CallbackType<T, boolean>): T {
+    if (whereFunc) return this.where(whereFunc).first();
+    return this[0];
+  }
+
+  public last(whereFunc?: CallbackType<T, boolean>): T {
+    if (whereFunc) return this.where(whereFunc).last();
+    const lastIndex = this.length ? this.length - 1 : 0;
+    return this[lastIndex];
+  }
+
+  public distinct<K = T>(selectFunc?: CallbackType<T, K>): LINQ<T> {
+    if (selectFunc)
+      return this.whereWithAccum(selectFunc, null, (m, i, linq, selected) => selected.indexOf(selected[i]) === i);
+    return this.where((m, i, linq) => linq.indexOf(m) === i);
+  }
+
+  public max(numberFunc?: CallbackType<T, number>): LINQ<T> {
+    return this.whereWithAccum(numberFunc, selected => Math.max(...selected), (m, i, linq, max) => m === max);
+  }
+
+  public maxValue(numberFunc?: CallbackType<T, number>): number {
+    return Math.max(...this.select(numberFunc));
+  }
+
+  public min(numberFunc?: CallbackType<T, number>): LINQ<T> {
+    return this.whereWithAccum(numberFunc, selected => Math.min(...selected), (m, i, linq, min) => m === min);
+  }
+
+  public minValue(numberFunc?: CallbackType<T, number>): number {
+    return Math.min(...this.select(numberFunc));
+  }
+
+  public ofType<Type extends new (...args) => any, K>(type: Type | Type[], selectFunc?: CallbackType<T, K>): LINQ<T> {
+    const types = Array.isArray(type) ? LINQ.from(type) : LINQ.from([type]);
+    return this.whereWithAccum(selectFunc, null, m => types.some(k => m instanceof k));
+  }
+
+  public orderByAscending<K>(sortSelectFunc?: CallbackOnlyItemType<T, K>): LINQ<T> {
+    if (sortSelectFunc)
+      return LINQ.from(
+        this.sort((a, b) => {
+          const ak = sortSelectFunc(a);
+          const bk = sortSelectFunc(b);
+          if (ak < bk) return -1;
+          if (ak > bk) return 1;
+          return 0;
+        }),
+      );
+    return LINQ.from(
+      this.sort((a, b) => {
+        if (a < b) return -1;
+        if (a > b) return 1;
+        return 0;
+      }),
+    );
+  }
+
+  public orderByDescending<K>(sortSelectFunc?: CallbackOnlyItemType<T, K>): LINQ<T> {
+    if (sortSelectFunc)
+      return LINQ.from(
+        this.sort((a, b) => {
+          const ak = sortSelectFunc(a);
+          const bk = sortSelectFunc(b);
+          if (ak > bk) return -1;
+          if (ak < bk) return 1;
+          return 0;
+        }),
+      );
+    return LINQ.from(
+      this.sort((a, b) => {
+        if (a > b) return -1;
+        if (a < b) return 1;
+        return 0;
+      }),
+    );
+  }
+
+  public sum(selectFunc?: CallbackType<T, number>): number {
+    if (selectFunc) return this.select(selectFunc).sum();
+    return this.reduce((a, b) => Number(a) + Number(b), 0);
+  }
+
+  public except<K>(items: T | T[] | LINQ<T>, selectFunc?: CallbackType<T, K>): LINQ<T> {
+    const array = Array.isArray(items) ? items : [items];
+    if (selectFunc) {
+      const linq = LINQ.from(array).select(selectFunc);
+      return this.where((m, i, l) => linq.indexOf(selectFunc(m, i, l)) === -1);
+    }
+    return this.where(m => array.indexOf(m) === -1);
+  }
+
+  public groupBy<K>(selectFunc: CallbackType<T, K>): collection.Dictionary<K, LINQ<T>> {
+    const dict = new collection.Dictionary<K, LINQ<T>>();
+    this.forEach((m, i) => {
+      const key = selectFunc(m, i, this);
+      if (!dict.containsKey(key)) dict.add(key, LINQ.from());
+      dict.get(key).push(m);
+    });
+    return dict;
+  }
+
+  public contains<K>(value: T, selectFunc?: CallbackOnlyItemType<T, K>): boolean {
+    if (selectFunc) return this.select(selectFunc).contains(selectFunc(value));
+    return this.indexOf(value) !== -1;
+  }
+
+  public containsAll<K>(value: T[] | LINQ<T>, selectFunc?: CallbackType<T, K>): boolean {
+    if (selectFunc) {
+      const selected = this.select(selectFunc);
+      return value.every((m, i) => selected.indexOf(selectFunc(m, i, this)) !== -1);
+    }
+    return value.every(m => this.indexOf(m) !== -1);
+  }
+
+  public average(selectFunc?: CallbackType<T, number>, whereFunc?: CallbackType<T, boolean>): number {
+    if (whereFunc) return this.where(whereFunc).average(selectFunc);
+    if (selectFunc) return this.select(selectFunc).average();
+    return this.sum() / this.count();
+  }
+
+  public intersect<K>(items: T | T[] | LINQ<T>, selectFunc?: CallbackType<T, K>): LINQ<T> {
+    const array = Array.isArray(items) ? items : [items];
+    if (selectFunc) {
+      const linq = LINQ.from(array).select(selectFunc);
+      return this.where((m, i, l) => linq.indexOf(selectFunc(m, i, l)) !== -1);
+    }
+    return this.where(m => array.indexOf(m) !== -1);
+  }
+
+  // Corrected return types for system Array<T> methods
+
+  public reverse(): LINQ<T> {
+    return super.reverse() as LINQ<T>;
+  }
+
+  public concat(...items: ConcatArray<T>[]): LINQ<T>;
+  public concat(...items: (T | ConcatArray<T>)[]): LINQ<T>;
+  public concat(...items: (T | ConcatArray<T> | LINQ<T>)[]): LINQ<T>;
+  public concat(...items: (T | ConcatArray<T> | LINQ<T>)[]): LINQ<T> {
+    return super.concat(...items) as LINQ<T>;
+  }
+
+  public slice(start?: number, end?: number): LINQ<T> {
+    return super.slice(start, end) as LINQ<T>;
+  }
+
+  public splice(start: number, deleteCount?: number): LINQ<T>;
+  public splice(start: number, deleteCount: number, ...items: T[]): LINQ<T>;
+  public splice(start: number, deleteCount: number, ...items: T[]): LINQ<T> {
+    return super.splice(start, deleteCount, ...items) as LINQ<T>;
+  }
+
+  public map<U>(callbackfn: (value: T, index: number, array: T[]) => U, thisArg?: any): LINQ<U> {
+    return super.map(callbackfn, thisArg) as LINQ<U>;
+  }
+
+  public filter<S extends T>(callbackfn: (value: T, index: number, array: T[]) => value is S, thisArg?: any): LINQ<S>;
+  public filter(callbackfn: (value: T, index: number, array: T[]) => unknown, thisArg?: any): LINQ<T>;
+  public filter(callbackfn: (value: T, index: number, array: T[]) => unknown, thisArg?: any): LINQ<T> {
+    return super.filter(callbackfn, thisArg) as LINQ<T>;
   }
 }
